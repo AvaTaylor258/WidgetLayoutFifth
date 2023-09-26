8e987572dee9f83e5d56d384a1079ec2743abad8
s10015164 <ava.taylor258@gmail.com>
2023-09-26 10:04:30 -0500

    Worked more with main.py and tried out ScrollView

--- main.py
+++ main.py
@@ -3,14 +3,20 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
+
+class GridLayoutExample(GridLayout):
+    pass
 
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.orientation = "lr-tb"
-        b = Button(text="A",size_hint=(.5,.5))
+        for letter in "abcdefghijklmnopqrstuvwxyz":
+            b = Button(text=f"Button {letter}",size_hint=(.2,.2))
+            self.add_widget(b)
 
 class BoxLayoutExample(BoxLayout):
     pass
